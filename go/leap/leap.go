79b48a693466208ae303fe20897b9843542044da
vidhlakh <vidhlakh@gmail.com>
2020-06-10 12:56:53 -0400

    Add: Is the year Leap year

---
+++ go/leap/leap.go
@@ -0,0 +1,13 @@
+// Package leap holds function leap
+package leap
+
+// IsLeapYear tells whether the given year is leap year or not
+func IsLeapYear(year int) bool {
+	if year%4 == 0 {
+		if year%100 == 0 && year%400 != 0 {
+			return false
+		}
+		return true
+	}
+	return false
+}
