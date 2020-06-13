0cd9bd68142e3f6f4b1c1d1b373619a8b73b1c98
vidhlakh <vidhlakh@gmail.com>
2020-06-13 11:37:43 -0400

    Add: Armstrong number, binary search

--- go/extrawork/work.go
+++ go/extrawork/work.go
@@ -1,7 +1,7 @@
-// Package armstrong has IsNumber function
-package armstrong
+package main
 
 import (
+	"fmt"
 	"math"
 )
 
@@ -11,20 +11,25 @@ func IsNumber(input int) bool {
 	count := float64(0)
 	var digit []float64
 	for input >= 1 {
+		fmt.Println("rem ", input%10)
 		digit = append(digit, float64(input%10))
 		input = input / 10
 		count++
 	}
-
+	fmt.Println("digit:", digit, count)
 	value := 0
 	var d float64
 	for _, d = range digit {
+		fmt.Println("d:", d, "math", math.Pow(d, count))
 		value += int(math.Pow(d, count))
-
+		fmt.Println("Value:", value)
 	}
 	if value == actual || count == 1 {
 		return true
 	}
 
 	return false
+}
+func main() {
+	fmt.Println("Is Armstrong number:", IsNumber(153))
 }
